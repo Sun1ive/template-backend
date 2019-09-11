284bae76e4bf71f66daa3fb5684842bdf2f966c4
s.ustinov <s.ustinov@officeukraine.fozzy.lan>
2019-09-11 11:24:19 +0300

    ci: removed cluster for heroku

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import 'reflect-metadata';
 
 import { ServerBuilder } from './factories/server';
 import { Config } from './config';
-import { cpus } from 'os';
+// import { cpus } from 'os';
 import { pid } from 'process';
 import { Server } from 'http';
-import cluster from 'cluster';
+// import cluster from 'cluster';
 import { initDB } from './db/connection';
 import { router as categoryRouter } from './routes/categories';
 import { router as productRouter } from './routes/products';
@@ -26,23 +26,24 @@ function buildServer(port: number | string = 3001): Server {
 
 (async () => {
 	if (process.env.NODE_ENV === 'production') {
-		if (cluster.isMaster) {
-			await initDB();
-			await seedDatabase();
-			for (let i = 0; i < cpus().length - 1; i++) {
-				const worker = cluster.fork();
-				worker.on('exit', () => {
-					console.log('Worker with pid %s crashed', worker.process.pid);
-					cluster.fork();
-				});
-			}
-		}
+		// if (cluster.isMaster) {
+		// 	await initDB();
+		// 	await seedDatabase();
+		// 	for (let i = 0; i < cpus().length - 1; i++) {
+		// 		const worker = cluster.fork();
+		// 		worker.on('exit', () => {
+		// 			console.log('Worker with pid %s crashed', worker.process.pid);
+		// 			cluster.fork();
+		// 		});
+		// 	}
+		// }
 
-		if (cluster.isWorker) {
-			await initDB();
-			process.on('unhandledRejection', console.error);
-			buildServer(Config.serverOptions.PORT);
-		}
+		// if (cluster.isWorker) {
+		await initDB();
+		await seedDatabase();
+		process.on('unhandledRejection', console.error);
+		buildServer(Config.serverOptions.PORT);
+		// }
 	} else {
 		await initDB();
 		await seedDatabase();
