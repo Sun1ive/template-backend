c242a562ccb1df2a8c2cbc33a85c979d2c0b6127
s.ustinov <s.ustinov@officeukraine.fozzy.lan>
2019-09-23 14:41:11 +0300

    feat: added tests for server

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -2,15 +2,32 @@ import { ServerBuilder } from '../src/factories/server';
 import { Express } from 'express-serve-static-core';
 
 describe('Test server factory', () => {
-  let server: Express | undefined = undefined;
+	let server: Express | undefined = undefined;
 
-  test('should create base instance', () => {
-    expect(server).toBeUndefined();
-    expect(typeof server).toEqual('undefined');
+	test('should create base instance', () => {
+		expect(server).toBeUndefined();
+		expect(typeof server).toEqual('undefined');
 
-    server = new ServerBuilder().build();
+		server = new ServerBuilder().build();
 
-    expect(server).not.toBeUndefined();
-    expect(typeof server).toEqual('function');
-  });
+		expect(server).not.toBeUndefined();
+		expect(typeof server).toEqual('function');
+
+		server = undefined;
+	});
+
+	test('should test all build functions', async () => {
+		expect(server).toBeUndefined();
+		expect(typeof server).toEqual('undefined');
+
+		server = new ServerBuilder()
+			.addCompression()
+			.addCors()
+			.addLogger('dev')
+			.addRoute('/hello', () => undefined)
+			.build();
+
+		expect(server).not.toBeUndefined();
+		expect(typeof server).toEqual('function');
+	});
 });
